480469853dc8cdffb4e60e8c1efc2bc772d57ed6
Rekha <rekhatiwari1006@gmail.com>
2019-01-15 12:54:27 +0530

    files added

--- invi_cards/urls.py
+++ invi_cards/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path('', home, name='home_page' )
+	path('', home, name='home_page' ),
+    path('E_Form/', Forms, name="E_Form")
+
 ]
